a0a7532c4f5b5e3abc84655537415144ae39d076
Sebin Chacko Mathew <sebinsebzz2002@gmail.com>
2021-10-03 18:37:22 +0530

    changed title and removed image

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// import App from "./App";
+
+// ReactDOM.render(<App />, document.getElementById("root"));
